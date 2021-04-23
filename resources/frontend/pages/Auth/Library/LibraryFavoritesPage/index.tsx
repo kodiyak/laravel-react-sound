7745690969cce3cff2ef8d1fcabf73969dd770ca
Math W <mathewsto51@gmail.com>
2021-04-23 16:33:24 -0300

    feat: library favorite songs page

--- resources/frontend/pages/Auth/Library/LibraryFavoritesPage/index.tsx
+++ resources/frontend/pages/Auth/Library/LibraryFavoritesPage/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import PageHeader from '../../../../components/Atoms/Headers/PageHeader'
 import ScrollPage from '../../../../components/Atoms/ScrollPage/index'
+import SongList from '../../../../components/Organisms/Song/SongList'
+import Col from '../../../../components/Utils/Col'
+import Row from '../../../../components/Utils/Row'
 
 const LibraryFavoritesPage: React.FC = () => {
-  return <ScrollPage>Library Favorites Page</ScrollPage>
+  return (
+    <ScrollPage>
+      <Row px={4}>
+        <PageHeader title="Your Favorite Songs" />
+      </Row>
+      <Col h={1500}>
+        <SongList />
+      </Col>
+    </ScrollPage>
+  )
 }
 
 export default LibraryFavoritesPage
