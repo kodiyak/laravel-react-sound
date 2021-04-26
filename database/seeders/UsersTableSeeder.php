b692e0d42d83e72c2b44bd88cf7bffb351fa4f8b
Math W <mathewsto51@gmail.com>
2021-04-26 16:15:17 -0300

    feat: api authentication

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'Mathews',
+            'username' => 'kodiyak',
+            'email' => 'mathewsto51@gmail.com',
+            'password' => Hash::make('s.o536123'),
+            'is_contributor' => true,
+        ]);
+    }
+}
